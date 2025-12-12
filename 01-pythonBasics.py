09cf25592308c09a999633873f1fcb70dfa0e2c7
Priyesh Singh <priyesh.code@gmail.com>
2025-12-12 09:58:47 +0530

    first commit

---
+++ 01-pythonBasics.py
@@ -0,0 +1,15 @@
+#1 Write a Python program to print "Hello priyesh" on the screen.
+print('hello priyeh')
+
+#2 Write a Python program that displays your name and age on the screen.
+name = 'priyesh'
+age = 11
+
+print(f'hello my name is {name} and I am {age} years old')
+
+#3 Write code to print all the pre-defined keywords in Python using the keyword library
+import keyword
+print(keyword.kwlist)
+
+#4 Write a program that checks if a given word is a Python keyword.
+word = (input)
